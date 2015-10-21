de0b02d65d8471d739dce5db1649d1d31d7cac3e
qq337255242 <yangkai28@sina.com>
2015-10-21 22:17:12 +0800

    Test1.1
    Test1.1

---
+++ KyangTest/KyangTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KyangTest
+//
+//  Created by Kyang on 15/10/21.
+//  Copyright © 2015年 Kyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
